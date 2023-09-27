387ebe7c5f6dabd51b692ab5dc55cd22ea91fc37
랑빛 <Rangbits@gmail.com>
2023-09-27 18:32:39 +0900

    230927 study

--- The Web Developer Bootcamp 2023-colt/app.js
+++ The Web Developer Bootcamp 2023-colt/app.js
@@ -40,3 +40,68 @@ switch (day) {
         console.log("non?");
         break;
 }
+
+// up down
+
+let maximum = parseInt(prompt("Enter the maximum number!"));
+
+while (!maximum) {
+    maximum = parseInt(prompt("Enter a valid number!"))
+}
+
+const targetNum = Math.floor(Math.random() * maximum) +1;
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    guess = parseInt(guess);
+    if (guess > targetNum) {
+        guess = prompt("Too high!")
+        attemts++;
+    } else if(guess < targetNum){
+        guess = prompt("Too low")
+        attemts++;
+    }else {
+        guess = prompt("Invalid guess")
+    }
+}
+
+if(guess === 'p'){
+    console.log("Quit")
+} else {
+    console.log("You Win");
+    console.log(`You got it ${attempts}`);
+}
+
+
+// Loop for of
+const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
+
+for (let i = 0; i < subreddits.length; i++) {
+    console.log(`visit reddit.com/r/${subreddits[i]}`);
+}
+
+for (let sub of subreddits){
+    console.log(`visit reddit.com/r/${sub}`);
+}
+
+// for in
+
+const testScores = {
+    keenan : 80,
+    damon : 67,
+    kim : 89,
+    shawn : 91,
+    marlon : 72,
+    dwayne : 77,
+    nadia : 83,
+    elvira : 97,
+    diedre : 81,
+    vonnie : 60
+}
+
+for (let person in testScores) {
+    console.log(`${person} scored ${testScores[person]}`);
+}
